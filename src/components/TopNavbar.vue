5bc517f769fb72837006cee7bffb1a55b9ddc22b
unknown <quiksniping1@gmail.com>
2022-06-13 06:47:53 -0400

    Fixed TopNavbar component to properly display, and fixed general templating within that. Changed navbar title to Zach's Portfolio

--- src/components/TopNavbar.vue
+++ src/components/TopNavbar.vue
@@ -1,30 +1,23 @@
 <template>
-  <v-card>
-    <v-layout>
-      <v-app-bar theme="dark" prominent>
-        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-app-bar-title>My Portfolio</v-app-bar-title>
-        <v-spacer></v-spacer>
-        <v-btn variant="text" icon="mdi-magnify"></v-btn>
-      </v-app-bar>
-      <v-navigation-drawer v-model="drawer" temporary>
-        <v-list>
-          <v-list-item
-            v-for="(item, i) in items"
-            :key="i"
-            :value="item"
-            :href="item.link"
-          >
-            <v-list-item-avatar>
-              <v-icon :icon="item.icon"></v-icon
-            ></v-list-item-avatar>
-            <v-list-item-title href="/">{{ item.title }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
-      <v-main style="height: 50rem"></v-main>
-    </v-layout>
-  </v-card>
+  <v-app-bar theme="dark" prominent>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar-title>Zach's Portfolio</v-app-bar-title>
+  </v-app-bar>
+  <v-navigation-drawer v-model="drawer" temporary>
+    <v-list>
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="i"
+        :value="item"
+        :href="item.link"
+      >
+        <v-list-item-avatar>
+          <v-icon :icon="item.icon"></v-icon
+        ></v-list-item-avatar>
+        <v-list-item-title href="/">{{ item.title }}</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <script>
